f2cf5087ad6dbae9d7f53800aff981428c09d3ea
Mathieu Urstein <mathieu.urstein@gmail.com>
2016-03-23 15:32:29 +0100

    Version approuvée avec Alain Hardy

--- src/CaesarProtocol/src/main/java/ch/heigvd/res/caesar/client/CaesarClient.java
+++ src/CaesarProtocol/src/main/java/ch/heigvd/res/caesar/client/CaesarClient.java
@@ -1,7 +1,11 @@
 package ch.heigvd.res.caesar.client;
 
 import ch.heigvd.res.caesar.protocol.Protocol;
+
+import java.io.*;
+import java.util.Scanner;
 import java.util.logging.Logger;
+import java.net.Socket;
 
 /**
  *
@@ -9,15 +13,59 @@ import java.util.logging.Logger;
  */
 public class CaesarClient {
 
-  private static final Logger LOG = Logger.getLogger(CaesarClient.class.getName());
+    private static final Logger LOG = Logger.getLogger(CaesarClient.class.getName());
 
-  /**
-   * @param args the command line arguments
-   */
-  public static void main(String[] args) {
-    System.setProperty("java.util.logging.SimpleFormatter.format", "[%1$tH:%1$tM:%1$tS::%1$tL] Client > %5$s%n");
-    LOG.info("Caesar client starting...");
-    LOG.info("Protocol constant: " + Protocol.A_CONSTANT_SHARED_BY_CLIENT_AND_SERVER);
-  }
-  
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.setProperty("java.util.logging.SimpleFormatter.format", "[%1$tH:%1$tM:%1$tS::%1$tL] Client > %5$s%n");
+        LOG.info("Caesar client starting...");
+
+        try {
+            ////// INITIALISATION
+            int offset;
+            Scanner scanner = new Scanner(System.in);
+
+            Socket serverSocket = new Socket("localhost", Protocol.PORT);
+            LOG.info("connected");
+            InputStream fromServer = serverSocket.getInputStream();
+            OutputStream toServer = serverSocket.getOutputStream();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(fromServer)
+            );
+            PrintWriter writer = new PrintWriter(toServer);
+            ////// DEBUT PROTOCOL
+            // 2 : client attends message server
+            System.out.println(reader.readLine());
+            // 3 : client donne la constante
+            String constant = scanner.nextLine();
+            offset = Integer.parseInt(constant);
+            LOG.info("Constante : "+offset);
+            writer.write(constant+"\n");
+            writer.flush();
+            LOG.info("Constante envoyée");
+            // 5 : serveur renvoie confirmation
+            System.out.println(reader.readLine());
+            // 7,10
+            String pulledString;
+            String sentString = "";
+            while (!sentString.equalsIgnoreCase("bye")) {
+                sentString = scanner.nextLine();
+                writer.write(Protocol.crypt(sentString, offset)+"\n");
+                writer.flush();
+
+                pulledString = reader.readLine();
+                LOG.info("Serveur a lu : "+pulledString+" > "+ Protocol.decrypt(pulledString, offset));
+            }
+
+            fromServer.close();
+            toServer.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            LOG.info("Server not found");
+        }
+    }
+
 }
--- src/CaesarProtocol/src/main/java/ch/heigvd/res/caesar/protocol/Protocol.java
+++ src/CaesarProtocol/src/main/java/ch/heigvd/res/caesar/protocol/Protocol.java
@@ -1,11 +1,46 @@
 package ch.heigvd.res.caesar.protocol;
 
+import java.text.Normalizer;
+
 /**
  *
  * @author Olivier Liechti
  */
 public class Protocol {
-  
-  public static final int A_CONSTANT_SHARED_BY_CLIENT_AND_SERVER = 42;
+
+    public static final int PORT = 22356;
+
+    // 1 : serveur demande la constante de offset
+    // 2 : client attends message server
+    // 3 : client donne la constante
+    // 4 : serveur inscrit la constant
+    // 5 : serveur renvoie confirmation
+
+    // 6 : Serveur : attend message
+    // 7 : Client  : envoie message (crypté)
+    // 8 : Serveur : reçoit message (décrypte)
+    // 9 : Serveur : renvoit message et se remet en 6:
+    // 10: Client reçoit message et se remet en 7 si OK:
+    // 10.1 : Si message différent de l'original : message ERREUR
+
+    public static String crypt(String msg, int offset) {
+        String s = "";
+        for(int i = 0; i < msg.length(); i++) {
+            char c = msg.charAt(i);
+
+            s += (char)(c - offset);
+        }
+        return s;
+    }
+
+    public static String decrypt(String msg, int offset) {
+        String s = "";
+        for(int i = 0; i < msg.length(); i++) {
+            char c = msg.charAt(i);
+
+            s += (char)(c + offset);
+        }
+        return s;
+    }
 
 }
